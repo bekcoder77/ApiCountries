b580aa8d963f75a4784a4725e90dd1e4b742fd93
bekcoder77 <127332753+bekcoder77@users.noreply.github.com>
2023-06-15 21:41:54 +0500

    Add files via upload

--- Api countries/js/script.js
+++ Api countries/js/script.js
@@ -8,8 +8,49 @@ const container = document.querySelector(".container");
 const navbar = document.querySelector(".navbar");
 const loader = document.querySelector(".loader");
 const countryModal = document.querySelector(".countryModal");
-
 const api_link = "https://restcountries.com/v3.1/all";
+
+const getCountry = async (idx) => {
+  console.log(idx);
+  const api_link = `https://restcountries.com/v3.1/translation/${idx}`;
+  let req = await fetch(api_link);
+  let datax = await req.json();
+  let data = datax[0];
+  navbar.classList.add("hidden");
+  countryModal.classList.remove("hidden");
+  countryModal.innerHTML = `
+  <div class="back">Back</div> 
+      <div class="modal">
+        <div class="leftModal">
+        <img src="${data.flags.png}"/>
+        </div>
+        <div class="rightModal">
+          <h4>${data.name.common}</h4>
+        <div class="fl-el">
+          <div class="innerLeft">
+            <p> <b>Population</b> :<span>${data.population}</span></p>
+            <p> <b>Region</b> :<span>${data.region}</span></p>
+            <p> <b>Subregion</b> :<span>${data.subregion}</span></p>
+            <p> <b>Capital</b> :<span class="capital">${data.capital}</span></p>
+          </div>
+          <div class="innerRight">
+              <p> <b>Timezones</b> :<span>${data.timezones}</span></p>
+            <p> <b>Continents</b> :<span>${data.continents}</span></p>
+            <p> <b>Fifa</b> :<span class="capital">${data.fifa}</span></p>
+            <p> <b>Languages</b> :<span class="capital">${data.tld}</span></p>
+          </div>
+        </div>
+
+        </div>
+      </div>
+`;
+  const back = countryModal.querySelector(".back");
+
+  back.addEventListener("click", () => {
+    countryModal.classList.add("hidden");
+  navbar.classList.remove("hidden");
+  });
+};
 
 const getData = async (api) => {
   loader.classList.add("act");
@@ -17,7 +58,7 @@ const getData = async (api) => {
   const data = await req.json();
   data.forEach((item) => {
     contires.innerHTML += `
-   <div class="box" onclick="getCountry(${item.idd.root})">
+   <div class="box" onclick="getCountry('${item.name.common}')">
    <img src="${item.flags.png} " alt="">
    <h1>${item.name.common}</h1>
    <div class="title">
@@ -31,6 +72,7 @@ const getData = async (api) => {
  </div>
    `;
   });
+
   loader.classList.remove("act");
 };
 getData(api_link);
